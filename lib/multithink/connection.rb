a7d33036d75ae589826f0431274de9497aafcf16
Joseph Glanville <joseph@cloudscaling.com>
2013-11-10 18:58:15 -0800

    Add some semblance of connection retryability

--- lib/multithink/connection.rb
+++ lib/multithink/connection.rb
@@ -6,20 +6,44 @@ include RethinkDB::Shortcuts
 
 class MultiThink::Connection
 
-  def initialize(servers)
+  DEFAULTS = {retries: 10}
+
+  def initialize(servers, options = {})
     @servers = servers
+    options = DEFAULTS.merge(options)
+    @retries = options.fetch(:retries)
     connect
   end
 
   def connect
     # TODO try all servers until we get a connection
     options = @servers.first
-    @conn = r.connect(options)
+    begin
+      @conn = r.connect(options)
+    rescue
+      @tried ||= 0
+      sleep 1
+      retry if (@tried += 1) < @retries
+    end
   end
 
   def run(query)
     # TODO handle connection failure
-    query.run(@conn)
+    begin
+      query.run(@conn)
+    rescue RuntimeError => e
+      reconnect
+    end
+  end
+
+  def reconnect
+    begin
+      # try fast path first
+      @conn.reconnect
+    rescue
+      # if that fails then try get a new connection
+      connect
+    end
   end
 
 end
